95b3f89c69d046b0be8c0bd09a9080dd64c500a2
Cenk Türkoğlu <122122850+cnktrkgl@users.noreply.github.com>
2025-03-26 23:29:22 +0300

    Add files via upload

---
+++ selector/script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("p").css("color", "red");
+    $("p").css("font-size", "20px");
+    $("#container").css("border", "1px solid black");
+    $("#container").css("padding", "10px");
+    $("#container").css("margin", "10px");
+    $("#container").css("width", "50%");
+    $("li").css("font-weight", "bold");
+    $("ul li").css("background", "lightgrey");
+    $("ul").css("list-style-type", "none");
+});
