341415e14a5e936e838be189ddb0d266dc64a018
Cenk Türkoğlu <122122850+cnktrkgl@users.noreply.github.com>
2025-03-28 17:41:08 +0300

    Add files via upload

---
+++ css_manipulasyon/script/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#btnChangeColor").click(function () {
+        $("#box1").css("background", "red");
+        $("#box2").css("background", "green");
+    });
+});
